c82f3f2981c9378c69b34d13a60d31dab1c7f450
wzshiming <wzshiming@foxmail.com>
2018-06-22 15:26:39 +0800

    adds xml methods

--- spec/xml.go
+++ spec/xml.go
@@ -28,3 +28,45 @@ type XML struct {
 	// The definition takes effect only when defined alongside type being array (outside the items).
 	Wrapped bool `json:"wrapped,omitempty"`
 }
+
+// WithName sets the xml name for the object
+func (x *XML) WithName(name string) *XML {
+	x.Name = name
+	return x
+}
+
+// WithNamespace sets the xml namespace for the object
+func (x *XML) WithNamespace(namespace string) *XML {
+	x.Namespace = namespace
+	return x
+}
+
+// WithPrefix sets the xml prefix for the object
+func (x *XML) WithPrefix(prefix string) *XML {
+	x.Prefix = prefix
+	return x
+}
+
+// AsAttribute flags this object as xml attribute
+func (x *XML) AsAttribute() *XML {
+	x.Attribute = true
+	return x
+}
+
+// AsElement flags this object as an xml node
+func (x *XML) AsElement() *XML {
+	x.Attribute = false
+	return x
+}
+
+// AsWrapped flags this object as wrapped, this is mostly useful for array types
+func (x *XML) AsWrapped() *XML {
+	x.Wrapped = true
+	return x
+}
+
+// AsUnwrapped flags this object as an xml node
+func (x *XML) AsUnwrapped() *XML {
+	x.Wrapped = false
+	return x
+}
