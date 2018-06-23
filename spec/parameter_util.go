f97e52acb53f809688589090c538477ca94df346
wzshiming <wzshiming@foxmail.com>
2018-06-23 16:49:59 +0800

    fixes

--- spec/parameter_util.go
+++ spec/parameter_util.go
@@ -8,34 +8,38 @@ const (
 )
 
 // QueryParam creates a query parameter
-func QueryParam(name string) *Parameter {
+func QueryParam(name string, schema *Schema) *Parameter {
 	p := &Parameter{}
 	p.In = InQuery
 	p.Name = name
+	p.Schema = schema
 	return p
 }
 
 // HeaderParam creates a header parameter, this is always required by default
-func HeaderParam(name string) *Parameter {
+func HeaderParam(name string, schema *Schema) *Parameter {
 	p := &Parameter{}
 	p.In = InHeader
 	p.Name = name
+	p.Schema = schema
 	return p
 }
 
 // PathParam creates a path parameter, this is always required
-func PathParam(name string) *Parameter {
+func PathParam(name string, schema *Schema) *Parameter {
 	p := &Parameter{}
 	p.In = InPath
 	p.Name = name
+	p.Schema = schema
 	return p
 }
 
 // CookieParam creates a path parameter, this is always required
-func CookieParam(name string) *Parameter {
+func CookieParam(name string, schema *Schema) *Parameter {
 	p := &Parameter{}
 	p.In = InCookie
 	p.Name = name
+	p.Schema = schema
 	return p
 }
 
@@ -48,6 +52,12 @@ func (h *Parameter) WithDescription(description string) *Parameter {
 // WithName a fluent builder method to override the name of the parameter
 func (p *Parameter) WithName(name string) *Parameter {
 	p.Name = name
+	return p
+}
+
+// WithSchema a fluent builder method to override the schema of the parameter
+func (p *Parameter) WithSchema(schema *Schema) *Parameter {
+	p.Schema = schema
 	return p
 }
 
