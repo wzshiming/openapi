2eb95a152e4320a19ec9478eac3479af363c76f2
wzshiming <wzshiming@foxmail.com>
2018-06-25 10:15:52 +0800

    modify refable

--- spec/refable.go
+++ spec/refable.go
@@ -1,10 +1,42 @@
 package spec
 
+import (
+	"encoding/json"
+	"errors"
+	"strings"
+)
+
 // Refable A simple object to allow referencing other components in the specification, internally and externally.
 // The Reference Object is defined by JSON Reference and follows the same structure, behavior and rules.
 // For this specification, reference resolution is accomplished as defined by the JSON Reference specification and not by the JSON Schema specification.
 type Refable struct {
 	// REQUIRED.
 	// The reference string.
+	Ref        string
+	Components string
+}
+
+// MarshalJSON returns m as the JSON encoding of m.
+func (m Refable) MarshalJSON() ([]byte, error) {
+	if m.Ref == "" {
+		return []byte("{}"), nil
+	}
+	return json.Marshal(ref{m.Components + m.Ref})
+}
+
+// UnmarshalJSON sets *m to a copy of data.
+func (m *Refable) UnmarshalJSON(data []byte) error {
+	if m == nil {
+		return errors.New("spec.Refable: UnmarshalJSON on nil pointer")
+	}
+	ref := ref{}
+	json.Unmarshal(data, &ref)
+	sp := strings.SplitN(ref.Ref, "/", 4)
+	m.Ref = sp[len(sp)-1]
+	m.Components = strings.Join(sp[:len(sp)-1], "/")
+	return nil
+}
+
+type ref struct {
 	Ref string `json:"$ref,omitempty"`
 }
--- spec/refable_util.go
+++ spec/refable_util.go
@@ -2,63 +2,72 @@ package spec
 
 // RefSchemas creates a ref schemas
 func RefSchemas(ref string) *Schema {
-	so := &Schema{}
-	so.Ref = "#/components/schemas/" + ref
-	return so
+	s := &Schema{}
+	s.Components = "#/components/schemas/"
+	s.Ref = ref
+	return s
 }
 
 // RefResponse creates a ref responses
 func RefResponse(ref string) *Response {
-	p := &Response{}
-	p.Ref = "#/components/responses/" + ref
-	return p
+	r := &Response{}
+	r.Components = "#/components/responses/"
+	r.Ref = ref
+	return r
 }
 
 // RefParameter creates a ref rarameter
 func RefParameter(ref string) *Parameter {
 	p := &Parameter{}
-	p.Ref = "#/components/parameters/" + ref
+	p.Components = "#/components/parameters/"
+	p.Ref = ref
 	return p
 }
 
 // RefExample creates a ref example
 func RefExample(ref string) *Example {
-	rb := &Example{}
-	rb.Ref = "#/components/examples/" + ref
-	return rb
+	e := &Example{}
+	e.Components = "#/components/examples/"
+	e.Ref = ref
+	return e
 }
 
 // RefRequestBody creates a ref request body
 func RefRequestBody(ref string) *RequestBody {
-	rb := &RequestBody{}
-	rb.Ref = "#/components/requestBodies/" + ref
-	return rb
+	r := &RequestBody{}
+	r.Components = "#/components/requestBodies/"
+	r.Ref = ref
+	return r
 }
 
 // RefHeader creates a ref header
 func RefHeader(ref string) *Header {
-	rb := &Header{}
-	rb.Ref = "#/components/headers/" + ref
-	return rb
+	h := &Header{}
+	h.Components = "#/components/headers/"
+	h.Ref = ref
+	return h
 }
 
 // RefSecurityScheme creates a ref security schemes
 func RefSecurityScheme(ref string) *SecurityScheme {
-	rb := &SecurityScheme{}
-	rb.Ref = "#/components/securitySchemes/" + ref
-	return rb
+	s := &SecurityScheme{}
+	s.Components = "#/components/securitySchemes/"
+	s.Ref = ref
+	return s
 }
 
 // RefLink creates a ref link
 func RefLink(ref string) *Link {
-	rb := &Link{}
-	rb.Ref = "#/components/links/" + ref
-	return rb
+	l := &Link{}
+	l.Components = "#/components/links/"
+	l.Ref = ref
+	return l
 }
 
 // RefCallback creates a ref callback
 func RefCallback(ref string) *Callback {
-	rb := &Callback{}
-	rb.Ref = "#/components/callbacks/" + ref
-	return rb
+	c := &Callback{}
+	c.Components = "#/components/callbacks/"
+	c.Ref = ref
+	return c
 }
