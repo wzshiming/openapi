6d0c1bfecb8ce1a471e75d0f61f438cdbfac0995
wzshiming <wzshiming@foxmail.com>
2019-03-31 00:09:49 +0800

    fixes

--- spec/security_scheme_util.go
+++ spec/security_scheme_util.go
@@ -1,16 +1,16 @@
 package spec
 
 const (
-	http          = "http"
-	apiKey        = "apiKey"
-	oauth2        = "oauth2"
-	openIdConnect = "openIdConnect"
+	SecurityHTTP          = "http"
+	SecurityAPIKey        = "apiKey"
+	SecurityOAuth2        = "oauth2"
+	SecurityOpenIDConnect = "openIdConnect"
 )
 
 // BasicAuth creates a basic auth security scheme
 func BasicAuth() *SecurityScheme {
 	ss := &SecurityScheme{}
-	ss.Type = http
+	ss.Type = SecurityHTTP
 	ss.Scheme = "basic"
 	return ss
 }
@@ -18,7 +18,7 @@ func BasicAuth() *SecurityScheme {
 // BearerAuth creates a bearer auth security scheme
 func BearerAuth(bearerFormat string) *SecurityScheme {
 	ss := &SecurityScheme{}
-	ss.Type = http
+	ss.Type = SecurityHTTP
 	ss.Scheme = "bearer"
 	ss.BearerFormat = bearerFormat
 	return ss
@@ -27,7 +27,7 @@ func BearerAuth(bearerFormat string) *SecurityScheme {
 // APIKeyAuth creates an api key auth security scheme
 func APIKeyAuth(fieldName, valueSource string) *SecurityScheme {
 	ss := &SecurityScheme{}
-	ss.Type = apiKey
+	ss.Type = SecurityAPIKey
 	ss.Name = fieldName
 	ss.In = valueSource
 	return ss
@@ -37,7 +37,7 @@ func APIKeyAuth(fieldName, valueSource string) *SecurityScheme {
 func OAuth2Implicit(authorizationURL string) *SecurityScheme {
 	ss := &SecurityScheme{}
 	ss.Flows = &OAuthFlows{}
-	ss.Type = oauth2
+	ss.Type = SecurityOAuth2
 	ss.Flows.Implicit.AuthorizationURL = authorizationURL
 	return ss
 }
@@ -46,7 +46,7 @@ func OAuth2Implicit(authorizationURL string) *SecurityScheme {
 func OAuth2Password(tokenURL string) *SecurityScheme {
 	ss := &SecurityScheme{}
 	ss.Flows = &OAuthFlows{}
-	ss.Type = oauth2
+	ss.Type = SecurityOAuth2
 	ss.Flows.Password.TokenURL = tokenURL
 	return ss
 }
@@ -55,7 +55,7 @@ func OAuth2Password(tokenURL string) *SecurityScheme {
 func OAuth2AuthorizationCode(tokenURL string) *SecurityScheme {
 	ss := &SecurityScheme{}
 	ss.Flows = &OAuthFlows{}
-	ss.Type = oauth2
+	ss.Type = SecurityOAuth2
 	ss.Flows.AuthorizationCode.TokenURL = tokenURL
 	return ss
 }
@@ -64,7 +64,7 @@ func OAuth2AuthorizationCode(tokenURL string) *SecurityScheme {
 func OAuth2ClientCredentials(authorizationURL, tokenURL string) *SecurityScheme {
 	ss := &SecurityScheme{}
 	ss.Flows = &OAuthFlows{}
-	ss.Type = oauth2
+	ss.Type = SecurityOAuth2
 	ss.Flows.ClientCredentials.AuthorizationURL = authorizationURL
 	ss.Flows.ClientCredentials.TokenURL = tokenURL
 	return ss
